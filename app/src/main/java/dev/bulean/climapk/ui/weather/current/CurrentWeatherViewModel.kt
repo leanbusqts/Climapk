88f9cc73ac4fefa3b25f7fe40033c2b7c2776af3
bulean <leanbusqts@gmail.com>
2020-10-30 14:49:22 -0300

    Initial commit

---
+++ app/src/main/java/dev/bulean/climapk/ui/weather/current/CurrentWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package dev.bulean.climapk.ui.weather.current
+
+import androidx.lifecycle.ViewModel
+
+class CurrentWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/dev/bulean/climapk/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package dev.bulean.climapk.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
